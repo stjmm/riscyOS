fda1bcb80d5979f6be3f22b3076a3eded04d675e
stjmm <shetrynajerkme@gmail.com>
2026-03-30 16:07:43 +0200

    first commit

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -xue
+
+QEMU=qemu-system-riscv32
+
+CC=clang
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fuse-ld=lld -fno-stack-protector -ffreestanding -nostdlib"
+
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf kernel.c
+
+$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
+    -kernel kernel.elf
